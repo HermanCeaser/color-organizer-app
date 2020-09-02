b829adce5a44d0f1d970caee125ec946fd59e91e
repl.it user <replituser@example.com>
2020-09-02 07:29:15 +0000

    Updates

---
+++ precache-manifest.fa88dbd9e140896cef4fcf5d363eac4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866d77096942b648144d44e0e9e62d01",
+    "url": "/color-organizer-app/index.html"
+  },
+  {
+    "revision": "a09e9c0cf8eacac8dfd3",
+    "url": "/color-organizer-app/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "66b63350b8b2ae70d142",
+    "url": "/color-organizer-app/static/css/main.a007447b.chunk.css"
+  },
+  {
+    "revision": "a09e9c0cf8eacac8dfd3",
+    "url": "/color-organizer-app/static/js/2.ea6e50d0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/color-organizer-app/static/js/2.ea6e50d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b63350b8b2ae70d142",
+    "url": "/color-organizer-app/static/js/main.fd2f4d5c.chunk.js"
+  },
+  {
+    "revision": "657cef6d6aaeeda87ea8",
+    "url": "/color-organizer-app/static/js/runtime-main.6e1612f9.js"
+  }
+]);
